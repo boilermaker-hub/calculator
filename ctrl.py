ac8e0f20228ec127d31b1b928f4a0c6874177c50
developer2 <dep2@email.com>
2025-12-24 17:26:01 +0900

    Modify ctrl.py to connect btn1 to calculate method

--- ctrl.py
+++ ctrl.py
@@ -1,4 +1,4 @@
- #ch 5.2.1  ctrl.py
+ #ch 6.2.2  ctrl.py
 
 class Control:
 
@@ -6,7 +6,10 @@ class Control:
         self.view=view
         self.connectSignals()
 
+    def calculate(self):
+        pass
+
     def connectSignals(self):
-        self.view.btn1.clicked.connect(self.view.activateMessage)
+        self.view.btn1.clicked.connect(self.calculate)
         self.view.btn2.clicked.connect(self.view.clearMessage)
         
